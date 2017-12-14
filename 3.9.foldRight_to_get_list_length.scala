946cf21a16687be89ecd3c10c3b9de871802fdd7
david barkhuizen <david@yoco.co.za>
2017-12-14 14:49:35 +0200

    3.8 & 3.9

---
+++ 3.9.foldRight_to_get_list_length.scala
@@ -0,0 +1,28 @@
+package fpinscala.examples
+
+sealed trait List[+A]
+case object Nil extends List[Nothing]
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+object List {
+
+	def apply[A](as: A*): List[A] =
+		if (as.isEmpty) Nil
+		else Cons(as.head, apply(as.tail: _*))
+}
+
+object Module {
+
+	def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B =
+
+		as match {
+			case Nil => z
+			case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+		}
+
+	def main(args : Array[String]) : Unit = {
+		
+		val result = foldRight(List(1,2,3), 0)((x, acc) => acc + x)
+		println(result)
+	}
+}
